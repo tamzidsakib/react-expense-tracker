ef52600800fc64c3ef3a8faf7f9cd46f28ec4549
tamzid sakib <tmsakib@gmail.com>
2022-12-08 15:41:30 +0600

    init

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import ExpensesFilter from "./ExpensesFilter";
+import "./Expenses.css";
+import Card from "../UI/Card";
+import ExpenseList from "./ExpenseList";
+
+const Expenses = (props) => {
+  const [selectedYear, setSelectedYear] = useState("2020");
+  const filterChangeHandler = (yearChange) => {
+    setSelectedYear(yearChange);
+  };
+
+  const filteredExpense = props.items.filter(expense=>{
+  return expense.date.getFullYear().toString()===selectedYear;
+  })
+
+  
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          onSelectedYear={selectedYear}
+          onFilterChange={filterChangeHandler}
+        />
+        <ExpenseList items={filteredExpense}/>
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import "./NewExpense.css";
+import ExpenseForm from "./ExpenseForm";
+
+const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    props.addNewExpense(expenseData);
+    setIsEditing(false);
+  };
+  const changeEditingHandler = () => {
+    setIsEditing(true);
+  };
+  const cancelEditingHandler = () => {
+    setIsEditing(false);
+  };
+  return (
+    <div className="new-expense">
+      {!isEditing && (
+        <button onClick={changeEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={cancelEditingHandler}
+        />
+      )}
+    </div>
+  );
+};
+
+export default NewExpense;
