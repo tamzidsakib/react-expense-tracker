2de1bfadfa690e6582fa5e69b6b69f01b4949406
tamzid sakib <tmsakib@gmail.com>
2022-12-08 16:06:35 +0600

    comment

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+// expense data
 const DUMMY_EXPENSES = [
   {
     id: "e1",
